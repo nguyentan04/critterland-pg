3b9db8c2924944dafa9a205ca20f352c0a47517d
themagicalfinance <100901957+themagicalfinance@users.noreply.github.com>
2022-06-05 21:24:05 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -310,8 +310,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e76b017092c3ab12d3c5cfbc90be6d6b",
-"/": "e76b017092c3ab12d3c5cfbc90be6d6b",
+"index.html": "721ca99d712521e178d90413f1adba0b",
+"/": "721ca99d712521e178d90413f1adba0b",
 "main.dart.js": "a609803f33b1aca1c06baa357cae5245",
 "manifest.json": "849d7aed2c3eef5eb615d782d8622af4",
 "version.json": "5fe19516353d9f088081f9868144bd62"
